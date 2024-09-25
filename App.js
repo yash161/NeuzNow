0f1e68b906eb49b6cd0908438763920c3808b6be
SHAH <yshah12@calstatela.edu>
2024-09-24 23:05:50 -0700

    adding login and signup page

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './core/theme'
 
 import DrawerNavigator from "./navigation/DrawerNavigator";
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+} from './screens/'
 
- const App = () => {
+// import {StartScreen} from './screens/StartScreen'
+const Stack = createStackNavigator()
+
+export default function App() {
   return (
-      <DrawerNavigator />
-  );
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="LoginScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={DrawerNavigator} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
-export default App;
